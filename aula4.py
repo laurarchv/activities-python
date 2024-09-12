c91e6f2235df04e6758a06ecf6ff281025cfdf92
laura!! <laurabezerraribeiro@gmail.com>
2024-09-12 09:18:15 -0300

    Add files via upload

---
+++ aula4.py
@@ -0,0 +1,16 @@
+# atribuição de variáveis
+nome = "Laura"
+idade = 15
+altura = 1.58
+
+# exibir mensagem na tela
+print("nome: ", nome, " idade: ", idade, "altura: ", altura)
+# formatação com f-string
+print(f"nome: {nome} idade: {idade} altura: {altura}")
+
+nome = input("Digite o se nome: ")
+idade = input("Digite a sua idade: ")
+print(type(idade))
+altura = float(input("Digite a sua altura: "))
+
+print(f"nome: {nome} idade: {idade} altura: {altura}")
